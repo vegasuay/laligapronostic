fc622748f0a7ed7b070be507f0468fae6ee8cd2b
roberto vega <rvsuay@indra.es>
2021-07-27 11:55:32 +0200

    clasif clas

---
+++ resources/clasif.py
@@ -0,0 +1,10 @@
+class Santander():
+
+    def __init__(self, team, pos, pts, pj, pg, pe, pp): 
+        self.team = team 
+        self.pos = int(pos)
+        self.pts = int(pts)
+        self.pj=int(pj)
+        self.pg=int(pg)
+        self.pe=int(pe)
+        self.pp=int(pp)
