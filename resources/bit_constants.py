93b7ea87d5c1496184314e79dc92be9f5ce190e5
roberto vega <rvsuay@indra.es>
2021-10-29 18:37:24 +0200

    new png

---
+++ resources/bit_constants.py
@@ -0,0 +1,26 @@
+BWIN = {
+    'GETAFE':'GETAFE CF',                
+    'LEVANTE':'LEVANTE UD',
+    'BETIS':'REAL BETIS',
+    'REAL MADRID':'REAL MADRID',
+    'ESPANOL':'RCD ESPANYOL BARCELONA',
+    'ATH MADRID':'ATLETICO MADRID',
+    'SOCIEDAD':'REAL SOCIEDAD',
+    'VALLECANO':'RAYO VALLECANO',
+    'GRANADA':'GRANADA CF',
+    'ATH BILBAO':'ATHLETIC BILBAO',
+    'CADIZ':'CADIZ CF',
+    'MALLORCA':'RCD MALLORCA',
+    'VILLARREAL':'VILLAREAL CF',
+    'VALENCIA':'VALENCIA CF',
+    'BARCELONA':'FC BARCELONA',
+    'ELCHE':'ELCHE CF',
+    'SEVILLA':'SEVILLA FC',
+    'CELTA':'RC CELTA DE VIGO',
+    'ALAVES':'DEPORTIVO ALAVES',
+    'OSASUNA':'CA OSASUNA',
+}
+
+BWIN_URL= 'https://sports.bwin.es/es/sports/f%C3%BAtbol-4/apuestas/espa%C3%B1a-28/laliga-102829'
+WILLIAM_URL = 'https://sports.williamhill.es/betting/es-es/football/competitions/OB_TY338/Espana-LaLiga-Santander/matches/OB_MGMB/Ganador-del-partido'
+POKER_URL = 'https://www.pokerstarssports.es/#/soccer/competitions/10679937'
